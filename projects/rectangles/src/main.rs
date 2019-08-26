64252386ea548b94dd4cb19cc0b2858255dbd113
Chan Y. Park <chan.splendid.park@gmail.com>
2019-08-26 16:53:06 +0900

    listing 5-13

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -4,28 +4,17 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-//    let width1 = 30;
-//    let height1 = 50;
-//    let rect1 = (30, 50);
     let rect1 = Rectangle { width: 30, height: 50 };
 
     println!(
         "The area of the rectangle is {:?} square pixels.",
-//        area(width1, height1)
-//        area(rect1)
-//        area(&rect1)
-        rect1
+        rect1.area()
     );
 }
-
-//fn area(width: u32, height: u32) -> u32 {
-//    width * height
-//}
-//
-//fn area(dimensions: (u32, u32)) -> u32 {
-//    dimensions.0 * dimensions.1
-//}
-//fn area(rectangle: &Rectangle) -> u32 {
-//    rectangle.width * rectangle.height
-//}
